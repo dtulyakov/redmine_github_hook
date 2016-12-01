7915adae255562b91049fe1a182c8ca34945e580
Денис Туляков <dtulyakov@gmail.com>
2016-12-01 10:23:40 +0300

    На ветке master 	изменено:      app/views/github_hook/welcome.html.erb 	изменено:      init.rb

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_github_hook do
   name 'Redmine Github Hook plugin'
-  author 'Jakob Skjerning'
+  author 'Denis Tulyakov'
   description 'This plugin allows your Redmine installation to receive Github post-receive notifications'
-  url 'https://github.com/koppen/redmine_github_hook'
-  author_url 'http://mentalized.net'
+  url 'https://github.com/dtulyakov/redmine_github_hook'
+  author_url 'http://blog.dtulyakov.ru'
   version RedmineGithubHook::VERSION
 end
